f5c970f39883c6872253dc27ec4bd68698094d34
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2024-05-27 13:09:01 +0300

    first use html-files

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+
+app.get('/', (req, res) => {	
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));    
+});
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'about.html'));
+})
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log('server in running on port ${PORT}');
-
-})
+    console.log('listening on port ${PORT}');});
