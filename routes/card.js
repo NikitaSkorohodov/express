a20eda6eebb0caa1b359c8ce053ab79a2c092ed8
NikitaSkorohodov <113201752+NikitaSkorohodov@users.noreply.github.com>
2024-06-04 12:02:35 +0300

    добавление в карзину и ее просмотор

--- routes/card.js
+++ routes/card.js
@@ -1,19 +1,22 @@
-const {Router} = require('express')
-const Card = require('../models/card')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require('express');
+const Card = require('../models/card');
+const Course = require('../models/course');
+const router = Router();
 
 router.post('/add', async (req, res) => {
-  const course = await Course.getById(req.body.id)
-  await Card.add(course)
-  res.redirect('/card')
-})
-router.get('/', async (req, res) => {
-  const card = await Card.fetch()
-  res.render('card',{
-    title: 'корзина',
-    card
-  })
-})
+  const course = await Course.getById(req.body.id);
+  await Card.add(course);
+  res.redirect('/card');
+});
 
-module.exports = router
+router.get('/', async (req, res) => {
+  const card = await Card.fetch();
+  res.render('card', {
+    title: 'корзина',
+    isCard: true,
+    courses: card.courses,
+    price: card.price
+  });
+});
+
+module.exports = router;
